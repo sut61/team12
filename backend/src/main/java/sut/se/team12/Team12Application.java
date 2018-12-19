ae17f004fccf0cbee2d8a18cb85323134d1e5cf4
taneuy <tanneuy@gmail.com>
2018-12-19 13:18:02 +0700

    add backend

---
+++ backend/src/main/java/sut/se/team12/Team12Application.java
@@ -0,0 +1,14 @@
+package sut.se.team12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team12Application.class, args);
+	}
+
+}
+
