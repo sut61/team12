dfd69f8985a92be38314916c00f75aab2117e2a4
sasiprapha220739 <bas22739@gmail.com>
2019-02-05 18:07:49 +0700

    close #72 - ทำEntity ของระบบย่อยจองล็อกเกอร์

---
+++ backend/src/main/java/sut/se/team12/entity/Locker.java
@@ -0,0 +1,23 @@
+package sut.se.team12.entity;
+
+
+import lombok.*;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Table(name = "tb_locker")
+
+
+public class Locker {
+    @Id
+    @SequenceGenerator(name="locker_seq",sequenceName="locker_seq")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="locker_seq")
+    @Column(name="LOCKER_ID")
+    private Long lockerId;
+    private String lockerName;
+}
