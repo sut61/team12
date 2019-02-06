5aa418703af671080527a1922cc30c6be930b917
rachataporn <stepme693@gmail.com>
2019-02-06 23:28:40 +0700

    ทำ test case ระบบแจ้งซ่อมอุปกรณ์ - close #83

--- backend/src/main/java/sut/se/team12/entity/Lease.java
+++ backend/src/main/java/sut/se/team12/entity/Lease.java
@@ -5,6 +5,9 @@ import lombok.*;
 import java.util.Date;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -20,21 +23,29 @@ public class Lease {
     @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="lease_seq")
     @Column(name="LEASE_ID")
     private Long leaseId;
+
+    @NotNull
+    @Size(min=5,max=30)
+    @Pattern(regexp="[a-zA-z0-9. ]+")
     private String note;
+
     
 
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = Admin.class)
     @JoinColumn(name = "ADMIN_ID", insertable = true)
     private  Admin admin;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = Member.class)
     @JoinColumn(name = "MEMBER_ID", insertable = true)
     private  Member member;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = LeaseAccessory.class)
     @JoinColumn(name = "LEASEACCEESSORY_ID", insertable = true)
     private  LeaseAccessory accessory;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = LeaseDuration.class)
     @JoinColumn(name = "LEASEDURATION_ID", insertable = true)
     private  LeaseDuration duration;
